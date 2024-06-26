c15b2184651c33f63e373bedd93ec8b20b712866
muthiikm <muthiikm@gmail.com>
2024-06-26 14:44:04 +0300

    view binding implementation

--- AndroidStudioProjects/fragments/app/src/main/java/com/fragments/MainActivity.kt
+++ AndroidStudioProjects/fragments/app/src/main/java/com/fragments/MainActivity.kt
@@ -4,21 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.fragments.ui.theme.FragmentsTheme
+import androidx.fragment.app.Fragment
+import com.fragments.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContent {
+        setContent {binding.root }
+
+        binding.fragmentBtn.setOnClickListener(){
+
+            replaceFragment(FirstFragment())
+
+
+        }
+
+        binding.fragmentBtn.setOnClickListener(){
+
+            replaceFragment(SecondFragment())
+
 
         }
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+//        fragmentTransaction.addToBackStack(null)  // Optional: adds this transaction to the back stack
+        fragmentTransaction.commit()  // Don't forget to commit the transaction
+    }
+
 }
